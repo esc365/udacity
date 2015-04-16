83ae7b29f4ffd293d45f9a258c3ad64b063b1f0c
esc365 <esc365@users.noreply.github.com>
2015-04-16 12:31:45 -0700

    first mini project done

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -25,10 +25,20 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 
 
-#########################################################
+
 ### your code goes here ###
 
+from sklearn.naive_bayes import GaussianNB
+t0 = time()
+clf = GaussianNB()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0,3), "s"
+predict = clf.predict(features_test)
 
-#########################################################
+
+from sklearn.metrics import accuracy_score
+
+print 'Accuracy of model: ', accuracy_score(labels_test, predict)
+
 
 
